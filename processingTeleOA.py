38b81ee0b5add068d94fcf27aff0946cde2a9102
Aidan <huang_ye@msn.com>
2017-12-12 16:45:40 +0800

    change type from 电信OA to 电信OA账号 change deptName from teleDept to idealDept now check leaders

--- processingTeleOA.py
+++ processingTeleOA.py
@@ -12,13 +12,33 @@ def find_the_first_not_none_value_upward(ws, row, column):
 		if ws[column + str(i)].value is not None:
 			return ws[column + str(i)]
 
+# 更改电信部门名称为公司部门名称
+def change_teleDept_to_idealDept(teleDept):
+	deptDict = {
+		"党群工作办公室/企业文化处/纪检监察室": "党群工作办公室",
+		"电信增值应用软件部": "增值应用业务部",
+		"公司领导": "公司总部",
+		"电信支撑软件部": "电信支撑软件部",
+		"计划财务处": "计划财务部",
+		"技术部": "技术质量管理部",
+		"人力资源库": "人力资源部"
+	}
+
+	if teleDept in deptDict:
+		return deptDict[teleDept]
+	else:
+		return teleDept
+
+
+
+
 # 电信公司OA对账文件
 filename = '17Q1.xlsx'
 
 # 核对账期 4位年份2位季度
 checkTime = '201704'
 # 核对账号类型
-accounttype = '电信OA'
+accounttype = '电信OA账号'
 
 wb = load_workbook(filename = filename)
 
@@ -70,9 +90,9 @@ for row in ws.rows:
 				usersStr= re.sub('admin_lxgs;', '', usersStr)
 				usersStr= re.sub(r'\d', '', usersStr)
 				usersStr= re.sub('_', '', usersStr)
-				if usersStr != "None" and departName not in ["工会", "团委", "财务处", "党委", "公司领导"]:
+				if usersStr != "None" and departName not in ["工会", "团委", "财务处", "党委"]:
 
-					# todo 部门名称改为台账系统部门名称
+					departName = change_teleDept_to_idealDept(departName)
 
 					users = usersStr.split(';')
 					for user in users:
