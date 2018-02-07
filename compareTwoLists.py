a88b542e2b5f41c26ab58ee98a562f2aec9fd2f0
Aidan <huang_ye@msn.com>
2018-02-07 15:38:41 +0800

    add some commit and regular codes

--- compareTwoLists.py
+++ compareTwoLists.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 from openpyxl import load_workbook
-from openpyxl.styles import Font, Color,colors
+from openpyxl.styles import Font,colors
 
 # 找第一个非空单元格
 def find_row_count_at_column(sheet, row, column):
@@ -64,7 +64,6 @@ try:
 except KeyError as e:
     print ('No sheet test:' + e.__str__())
 
-
 wss = wb[SRC_SHEET_NAME]
 
 # 创建比较结果表单
@@ -79,98 +78,63 @@ for i in range(1, COLUMN_END):
 # 当前行号
 currentRow = 2
 
+# 获取源数据总数，目标数据总数
 srcRowCount = find_row_count_at_column(wss, FIRST_LINE, COLUMN_SRC)
 desRowCount = find_row_count_at_column(wss, FIRST_LINE, COLUMN_DES)
 
-# 读取源数据进数据字典
-
+# 源数据，目标数据字典
 srcDict = {}
 desDict = {}
 
+# 读取源数据进数据字典
 for i in range(FIRST_LINE, FIRST_LINE + srcRowCount):
-
-    # srcData = []
-    # for j in range(1, COUNT_ALL + 1):
-    #     srcData.append(wss.cell(row=i, column=j).value)
-
     srcDict[wss.cell(row=i, column=COLUMN_SRC).value] = row_to_list(wss, i, COLUMN_SRC, COUNT_ALL)
 
-print (srcDict)
+# print (srcDict)
 
 # 读取对比数据进数据字典
-
 for i in range(FIRST_LINE, FIRST_LINE + desRowCount):
-    # desData = []
-    # for j in range(COLUMN_DES, COLUMN_END):
-    #
-    #     desData.append(wss.cell(row=i, column=j).value)
-
     desDict[wss.cell(row=i, column=COLUMN_DES).value] = row_to_list(wss, i, COLUMN_DES, COUNT_ALL)
 
-print (desDict)
-
+# print (desDict)
 
 # 循环源数据字典，在对比数据字典中找到相同的，则拷贝到新的结果页，同时在源数据字典和目标数据字典中都删除
 
 count_same = 0
 
 for key in srcDict.copy():
-    # print (key, 'corresponds to', srcDict[key])
 
     if key in desDict.keys():
 
         count_same += 1
 
-        # for j in range(1, COUNT_ALL + 1):
-            # wsr.cell(row=currentRow, column=j).value = srcDict[key][j-1]
-
+        # 相同数据写入目标页
         list_to_row(srcDict[key], wsr, currentRow, COLUMN_SRC)
-
-
-        # for j in range(COLUMN_DES, COLUMN_END):
-        #     wsr.cell(row=currentRow, column=j).value = desDict[key][j-COLUMN_DES]
-
         list_to_row(desDict[key], wsr, currentRow, COLUMN_DES)
 
-
+        # 源数据、目标数据字典内去除相同数据
         del srcDict[key]
         del desDict[key]
+
         currentRow += 1
 
 print ("There are " + str(count_same) + " same records")
 print ("There are " + str(srcDict.__len__()) + " records can't find in des")
 print ("There are " + str(desDict.__len__()) + " records can't find in src")
 
-wsr.cell(row=currentRow, column=COLUMN_SRC).value = "差异"
+wsr.cell(row=currentRow, column=COLUMN_SRC).value = "差异数据"
 wsr.cell(row=currentRow, column=COLUMN_SRC).font = Font(bold=True, color=colors.RED)
+
 currentRow += 1
 
 for key in srcDict.keys():
 
-    # for j in range(1, COUNT_ALL + 1):
-    #     wsr.cell(row=currentRow, column=j).value = srcDict[key][j-1]
-
     list_to_row(srcDict[key], wsr, currentRow, COLUMN_SRC)
-
     currentRow += 1
 
 for key in desDict.keys():
-    # for j in range(COLUMN_DES, COLUMN_END):
-    #     wsr.cell(row=currentRow, column=j).value = desDict[key][j-COLUMN_DES]
-
     list_to_row(desDict[key], wsr, currentRow, COLUMN_DES)
-
     currentRow += 1
-
-# print (srcDict)
-# print (desDict)
-
-# print (str(srcRowCount))
-# print (str(desRowCount))
-
-
-# print(wss.cell(row=1, column=1).value)
 
 wb.active = RESULT_SHEET_NUMBER
 wb.save(FILE_NAME)
-
