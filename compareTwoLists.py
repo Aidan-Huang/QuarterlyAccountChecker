2be332fd34f4dd82fd9d795b3cfc207581196f40
Aidan <huang_ye@msn.com>
2018-02-09 13:46:58 +0800

    refactor

--- compareTwoLists.py
+++ compareTwoLists.py
@@ -21,38 +21,32 @@ def list_to_row(data, sheet, row, column):
 def row_to_list(sheet, row, column, count):
     resList = []
     for i in range(column, column + count):
-        resList.append(sheet.cell(row=row, column=i).value)
+        resList.append(str(sheet.cell(row=row, column=i).value).strip())
     return resList
 
 # 比较文件
-FILE_NAME = 'q4.xlsx'
-
+FILE_NAME = 'q1.xlsx'
 # 源表单名称
 SRC_SHEET_NAME = 'Sheet1'
-
 # 比较结果表单
 COMPARE_RESULT = 'result'
-
 # 核对结果表单位置
 RESULT_SHEET_NUMBER = 1
-
 # 首行号
 FIRST_LINE = 2
-
-# 源列号
-COLUMN_SRC = 1
-
-# 关键字列数
-COUNT_KEY = 1
-
 # 总数据列数
 COUNT_ALL = 4
-
+# 关键字列数
+COUNT_KEY = 2
+# 源列号
+COLUMN_SRC = 1
 # 目标列号 总数据列加1再加上1个分隔列
 COLUMN_DES = COUNT_ALL + 1 + 1
-
 # 列尾号
 COLUMN_END = COUNT_ALL * 2 + 2
+
+# 分隔符
+DIV_CHAR = "&&"
 
 # 加载文件
 wb = load_workbook(filename = FILE_NAME)
@@ -88,30 +82,27 @@ desDict = {}
 
 # 读取源数据进数据字典
 for i in range(FIRST_LINE, FIRST_LINE + srcRowCount):
-    srcDict[wss.cell(row=i, column=COLUMN_SRC).value] = row_to_list(wss, i, COLUMN_SRC, COUNT_ALL)
-
+    key = DIV_CHAR.join(row_to_list(wss, i, COLUMN_SRC, COUNT_KEY))
+    srcDict[key] = row_to_list(wss, i, COLUMN_SRC, COUNT_ALL)
 # print (srcDict)
 
 # 读取对比数据进数据字典
 for i in range(FIRST_LINE, FIRST_LINE + desRowCount):
-    desDict[wss.cell(row=i, column=COLUMN_DES).value] = row_to_list(wss, i, COLUMN_DES, COUNT_ALL)
-
+    key = DIV_CHAR.join(row_to_list(wss, i, COLUMN_DES, COUNT_KEY))
+    desDict[key] = row_to_list(wss, i, COLUMN_DES, COUNT_ALL)
 # print (desDict)
 
-# 循环源数据字典，在对比数据字典中找到相同的，则拷贝到新的结果页，同时在源数据字典和目标数据字典中都删除
-
+# 相同数据数
 count_same = 0
 
+# 循环源数据字典，在对比数据字典中找到相同的，则拷贝到新的结果页，同时在源数据字典和目标数据字典中都删除
 for key in srcDict.copy():
-
     if key in desDict.keys():
-
+        # 相同数据数加1
         count_same += 1
-
         # 相同数据写入目标页
         list_to_row(srcDict[key], wsr, currentRow, COLUMN_SRC)
         list_to_row(desDict[key], wsr, currentRow, COLUMN_DES)
-
         # 源数据、目标数据字典内去除相同数据
         del srcDict[key]
         del desDict[key]
@@ -128,7 +119,6 @@ wsr.cell(row=currentRow, column=COLUMN_SRC).font = Font(bold=True, color=colors.
 currentRow += 1
 
 for key in srcDict.keys():
-
     list_to_row(srcDict[key], wsr, currentRow, COLUMN_SRC)
     currentRow += 1
 
